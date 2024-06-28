cb9a299828da471a6b0e03b12ff6faed6dd31b42
Liam Howell <liamhowell4@gmail.com>
2024-06-28 15:03:53 -0500

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Liam Howell on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
